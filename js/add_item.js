dbe5f536b79f2d3fef3803907a3427622685acea
Hams Gelban <143344135+Hams-hg2104047@users.noreply.github.com>
2024-03-27 21:33:11 +0300

    add item code

--- js/add_item.js
+++ js/add_item.js
@@ -1,42 +1,17 @@
 const itemsURL = "../json/items.json";
-let items = [];
+let items = JSON.parse(localStorage.getItem("items")) || []; 
 
-const uploadForm = document.querySelector("upload-form");
+const uploadForm = document.querySelector("#upload-form");
 
-uploadForm.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const item = formToObject(e.target);
-  //   item._id = Date.now();
-  setItemArtiest(item);
-  let validating = true;
-  for (const [k, value] of Object.entries(item)) {
-    if (value == "") {
-      validating = false;
-      alert("Please fill all field");
-      return;
-    }
-  }
 
-  const exist = items.findIndex((i) => b.is == item.id);
-  if (exist != -1) {
-    //handle update
-    items[exist] = item;
-  } else {
-    items.push(item);
-  }
-
-  localStorage.setItem("items", JSON.stringify(items));
-  e.target.reset();
-}
 
 function setItemArtiest(item) {
   const artists = JSON.parse(localStorage.getItem("users"));
+  console.log(artists)
   const loggedInArtist = artists.find((u) => u.isLoggedIn === true);
-  item._id = Date.now();
+  item.id = Date.now();
   item.currency = "QAR";
-  item.quantity_to_buy = 0;
   item.artist = loggedInArtist.name;
   item.artistID = loggedInArtist.id;
   loggedInArtist.itemsOnSale.push(item);
@@ -48,5 +23,57 @@ function formToObject(form) {
   for (const [e, value] of formData) {
     data[e] = value;
   }
+  console.log(data); 
   return data;
 }
+
+
+function updateItem(id) {
+  const item = items.find(i => i.id == id);
+  if (item) {
+    Object.entries(item).forEach(([key, value]) => {
+      const element = document.querySelector(`#${key}`);
+      if (element) {
+        if (key !== "quantity") {
+          element.setAttribute("readonly", true);
+        }
+        element.value = value;
+      }
+    });
+  } else {
+    console.log("Item not found");
+  }
+}
+
+
+function handleSubmit(e) {
+  alert("Start submiting")
+  console.log("Inside handle submission")
+  e.preventDefault();
+  console.log("Form submitted");
+  const item = formToObject(e.target);
+  setItemArtiest(item);
+  let validating = true;
+  for (const [k, value] of Object.entries(item)) {
+    if (value == "") {
+      validating = false;
+      alert("Please fill all fields");
+      return;
+    }
+  }
+  const exist = items.findIndex((i) => i.id == item.id);
+  if (exist != -1) {
+    //handle update
+    updateItem(item.id)
+    alert("Already there !");
+    items[exist] = item;
+  } else {
+    items.push(item); 
+  }
+
+  localStorage.setItem("items", JSON.stringify(items));
+  window.location.href = "/html/all_Items.html"
+}
+
+uploadForm.addEventListener("submit", handleSubmit);
+
